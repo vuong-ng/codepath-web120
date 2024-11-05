93291674c3a90320846bac43b08c6718cd0588ae
vuong-ng <nguyenxuanvuong107003@gmail.com>
2024-11-05 02:01:52 -0500

    add project crewmate

---
+++ crewmate/crewmate/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome, Vuong</h1>
+      <h2>Here is the Home Page</h2>
+    </>
+  )
+}
+
+export default App
